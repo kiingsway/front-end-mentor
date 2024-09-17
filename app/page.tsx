1977106ddf1e22fd1353269b97f634d6745bcd5e
King Sway (Marcos Oranmiyan) <knucles0906@live.com>
2024-09-16 20:27:58 -0400

    Recipe Page
    React -> HTML + CSS

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import RecipePage from "@/components/RecipePage";
-
-export default function Home() {
-  return (
-    <RecipePage />
-  );
-}
--- components/RecipePage/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function RecipePage(): JSX.Element {
-  return (
-    <div>RecipePage</div>
-  );
-}
