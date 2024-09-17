386206b97af54214ea258287593c4e5c930ac37a
King Sway (Marcos Oranmiyan) <knucles0906@live.com>
2024-09-16 20:17:16 -0400

    Recipe Page 0.1
    Starting Challenge: Recipe Page (https://www.frontendmentor.io/challenges/recipe-page-KiTsR8QQKm)

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import RecipePage from "@/components/RecipePage";
+
+export default function Home() {
+  return (
+    <RecipePage />
+  );
+}
---
+++ components/RecipePage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function RecipePage(): JSX.Element {
+  return (
+    <div>RecipePage</div>
+  );
+}
